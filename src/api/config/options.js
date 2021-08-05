6d1ca14a168912ffc632091e68921cbe6d4a1c28
rafaelmaia8384 <rafaelpvm@hotmail.com>
2021-08-05 17:20:10 -0300

    remove keys

--- src/api/config/options.js
+++ src/api/config/options.js
@@ -6,8 +6,8 @@ const OPT_JWT_SECRET = 'jwt-campeonato-de-piadas-secret';
 const OPT_UPLOAD_BUCKET = 'webcontent';
 const OPT_APP_NAME = 'campeonato-de-piadas';
 const OPT_S3_END_POINT = 'sfo2.digitaloceanspaces.com'
-const OPT_S3_ACCESS_KEY = 'MLMK6NTYYC5242Y3VAWU';
-const OPT_S3_SECRET_KEY = '52OSbfaK1/4V6xPYyokT3AxrSym8sCEUvaG5rEX5IsI';
+const OPT_S3_ACCESS_KEY = '';
+const OPT_S3_SECRET_KEY = '';
 
 module.exports = {
     optJwtSecret: OPT_JWT_SECRET,
